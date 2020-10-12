3b0924f297e308fa91e77f129991bf1db271e529
ujicobake102 <72608707+ujicobake102@users.noreply.github.com>
2020-10-12 21:42:38 +0800

    Update __init__.py

--- xxbbnn/__init__.py
+++ xxbbnn/__init__.py
@@ -10,7 +10,7 @@ import logging
 if bool(os.environ.get("ENV", False)):
     from xxbbnn.sample_config import Config
 else:
-    from xxbbnn.config import Config
+    from xxbbnn.bbbconfig import Config
 from logging.handlers import RotatingFileHandler
 
 # TODO: is there a better way?
