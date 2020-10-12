8679863a89555ed56d2854a6dcdecb90b00d7ab9
ujicobake102 <72608707+ujicobake102@users.noreply.github.com>
2020-10-12 21:47:34 +0800

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install rclone static binary
-curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
-unzip rclone-current-linux-amd64.zip
+#curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+#unzip rclone-current-linux-amd64.zip
 
 # Install aria2c static binary
 wget -q https://github.com/P3TERX/aria2-builder/releases/download/1.35.0_2020.09.04/aria2-1.35.0-static-linux-amd64.tar.gz
@@ -12,7 +12,7 @@ tar xf aria2-1.35.0-static-linux-amd64.tar.gz
 wget -q https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz
 tar xvf ffmpeg-git-amd64-static.tar.xz
 
-mv rclone-*-linux-amd64/rclone aria2c ffmpeg-*-amd64-static/ffmpeg ffmpeg-*-amd64-static/ffprobe /usr/local/bin/
+mv aria2c ffmpeg-*-amd64-static/ffmpeg ffmpeg-*-amd64-static/ffprobe /usr/local/bin/
 
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht.dat
 wget -q https://github.com/P3TERX/aria2.conf/raw/master/dht6.dat
